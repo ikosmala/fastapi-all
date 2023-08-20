2127cf430ac8015f45a1bb7de5d24c552fc03dcb
Iwo <kosmala.iwo@gmail.com>
2023-08-20 14:41:49 +0200

    added CORS configuration

--- app/main.py
+++ app/main.py
@@ -3,11 +3,20 @@ from . import models
 from .config import settings
 from .database import engine
 from .routers import post, user, auth, vote
+from fastapi.middleware.cors import CORSMiddleware
 
 # done by alembic
 # models.Base.metadata.create_all(bind=engine)
 
 app = FastAPI()
+
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=["*"],
+    allow_credentials=True,
+    allow_methods=["*"],
+    allow_headers=["*"],
+)
 
 app.include_router(post.router)
 app.include_router(user.router)
