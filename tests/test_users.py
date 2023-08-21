47505b4486d125194ca7017e84c2722abed8e967
Iwo <kosmala.iwo@gmail.com>
2023-08-21 22:35:28 +0200

    new tests for user

--- tests/test_users.py
+++ tests/test_users.py
@@ -1,35 +1,17 @@
-from fastapi.testclient import TestClient
-from app.main import app
 from app import schemas
-from app.config import settings
-from sqlalchemy import create_engine
-from sqlalchemy.orm import sessionmaker, declarative_base
-from app.database import get_db, Base
+from .database import client, session
 import pytest
-
-SQLALCHEMY_DATABASE_URL = f"postgresql://{settings.database_username}:{settings.database_password}@{settings.database_hostname}:{settings.database_port}/{settings.database_name}_test"
-
-engine = create_engine(SQLALCHEMY_DATABASE_URL)
-
-TestingSessionLocal = sessionmaker(autocommit=False, autoflush=False, bind=engine)
-
-
-def override_get_db():
-    db = TestingSessionLocal()
-    try:
-        yield db
-    finally:
-        db.close()
-
-
-app.dependency_overrides[get_db] = override_get_db
+from fastapi import HTTPException
 
 
 @pytest.fixture()
-def client():
-    Base.metadata.create_all(bind=engine)
-    yield TestClient(app)
-    Base.metadata.drop_all(bind=engine)
+def test_user(client):
+    user_data = {"email": "sometest@email.com", "password": "testpassword"}
+    response = client.post("/users/", json=user_data)
+    assert response.status_code == 201
+    new_user = response.json()
+    new_user["password"] = user_data["password"]
+    return new_user
 
 
 def test_root(client):
@@ -39,8 +21,37 @@ def test_root(client):
 
 def test_create_user(client):
     response = client.post(
-        "/users", json={"email": "test2@mail.com", "password": "testpass"}
+        "/users/", json={"email": "test2@mail.com", "password": "testpass"}
     )
     new_user = schemas.UserOut(**response.json())
     assert new_user.email == "test2@mail.com"
     assert response.status_code == 201
+
+
+def test_create_same_user_twice(client, test_user):
+    response = client.post(
+        "/users/", json={"email": test_user["email"], "password": test_user["password"]}
+    )
+    assert response.status_code == 409
+    assert "already exists" in response.json().get("detail")
+
+
+@pytest.mark.parametrize(
+    "email, password, result",
+    [
+        ("valid@email.com", "validpassword", 201),
+        ("invalid-email.com", "validpassword", 422),
+        ("valid@email.com", "", 422),
+    ],
+)
+def test_invalid_user_data(client, email, password, result):
+    response = client.post("/users/", json={"email": email, "password": password})
+    assert response.status_code == result
+
+
+def test_user_login(client, test_user):
+    response = client.post(
+        "/login",
+        data={"username": test_user["email"], "password": test_user["password"]},
+    )
+    assert response.status_code == 200
