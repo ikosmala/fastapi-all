8818d8aba7519651b18ee6f7ea8ba66a06da5824
Iwo <kosmala.iwo@gmail.com>
2023-08-23 18:22:52 +0200

    test

--- tests/conftest.py
+++ tests/conftest.py
@@ -8,7 +8,7 @@ from app.database import get_db, Base
 import pytest
 from app.oath2 import create_access_token
 
-SQLALCHEMY_DATABASE_URL = f"postgresql://testname:postgres@localhost:5432/test-db"
+SQLALCHEMY_DATABASE_URL = f"postgresql://postgres:testpassword@localhost:5432/test-db"
 
 engine = create_engine(SQLALCHEMY_DATABASE_URL)
 
