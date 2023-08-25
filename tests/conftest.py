669705391276e81f92fdc0a621866544a408aa28
Iwo <kosmala.iwo@gmail.com>
2023-08-25 10:00:07 +0200

    changed credentials

--- tests/conftest.py
+++ tests/conftest.py
@@ -8,7 +8,9 @@ from app.database import get_db, Base
 import pytest
 from app.oath2 import create_access_token
 
-SQLALCHEMY_DATABASE_URL = f"postgresql://postgres:postgres@localhost:5432/test-db"
+SQLALCHEMY_DATABASE_URL = (
+    f"postgresql://postgres:postgres@localhost:5432/fastapi-all_test"
+)
 
 engine = create_engine(SQLALCHEMY_DATABASE_URL)
 
